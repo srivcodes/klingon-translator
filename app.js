c036503b0b3a70c9147098fd7bb02ddc522d10cc
srividyamvenkatesh <srividyamvenkatesh@gmail.com>
2020-11-29 21:00:56 +0530

    Not working

---
+++ app.js
@@ -0,0 +1,34 @@
+var recommendbutton= document.querySelector("#btn-recommend");
+var inputtext= document.querySelector("#input-txt");
+var outputtext=document.querySelector("#output-txt");
+
+var serverurl="https://api.spotify.com.";
+
+function recommendurl(text){
+    return serverurl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+// outputtext.innerText ="sriv";
+
+//console.log(inputtext);
+
+recommendbutton.addEventListener("click", eventHandler)
+
+function eventHandler(){
+
+    var txtip = inputtext.value;
+    fetch(recommendurl(txtip))
+    .then(response => response.json())
+    console.log(response.json);
+    // .then(json => {
+
+    //     var recommendedsong = json.contents;
+    //     outputDiv.innerText = recommendedsong;
+    // })
+    // .catch(errorHandler)
+
